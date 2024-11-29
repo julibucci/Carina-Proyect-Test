ec1b8b08427038b73f4ae2b78fda076e1d92f026
Ivana Rosario <ivarsaval@gmail.com>
2024-11-29 00:39:49 -0300

    minor modifications

--- project-carina/src/main/java/org/example/carina/demo/mobile/gui/pages/HomePage.java
+++ project-carina/src/main/java/org/example/carina/demo/mobile/gui/pages/HomePage.java
@@ -3,25 +3,17 @@ package org.example.carina.demo.mobile.gui.pages;
 import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class HomePage extends AbstractPage {
 
     public HomePage(WebDriver driver) {
         super(driver);
-
     }
 
-    // Locator para el botón "Sign in"
     @FindBy(css = "a.login")
     private ExtendedWebElement loginButton;
 
-    // Método para hacer clic en el botón "Sign in" y navegar a LoginPage
     public LoginPage clickLogin() {
         loginButton.click();
         return new LoginPage(getDriver());
